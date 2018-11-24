dab2ec783a96579ec5a08a1bb771e830f9633fbd
Zhi Rui Tam <theblackcat@Zhis-MacBook-Pro.local>
2018-11-25 02:17:57 +0800

    update evaluate error prob2

--- HW2/prob2.py
+++ HW2/prob2.py
@@ -68,8 +68,8 @@ def test_run():
     x_train = x_train.reshape(len(x_train), 3, 32, 32).astype(dtype=np.float64)
     x_test = x_test.reshape(len(x_test), 3, 32, 32).astype(dtype=np.float64)
 
-    x_train = x_train / 255.0
-    x_test = x_test / 255.0
+    x_train = (x_train -  122.5 ) / 255.0
+    x_test = (x_test - 125.5) / 255.0
 
     print('x_train shape:', x_train.shape)
     print(x_train.shape[0], 'train samples')
